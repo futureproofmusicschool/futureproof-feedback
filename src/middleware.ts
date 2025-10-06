84a7dc4993e26a0c53cd836b98cd9abb9565b210
johnvonFMS <johnvon@futureproofmusicschool.com>
2025-10-05 17:12:05 -0700

    Fix: Allow static image files to bypass middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -42,6 +42,6 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:jpg|jpeg|png|svg|gif|webp|ico)).*)'],
 };
 
